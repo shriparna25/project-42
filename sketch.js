d700d695a8d08240027e0dd4a2b9b65349f18d88
shriparna25 <70945784+shriparna25@users.noreply.github.com>
2021-01-06 20:54:20 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,7 +3,9 @@ var scAngle, hrAngle, mnAngle;
 
 function setup() {
   createCanvas(800,800);
-  //sc = createSprite(400, 200, 50, 50);
+
+  angleMode(DEGREES);
+
 }
 
 function draw() {
@@ -14,24 +16,17 @@ mn = minute();
 sc = second();
 
 translate(width / 2, height / 2);
-angleMode(DEGREES);
+rotate(-90)
 
 scAngle = map(sc, 0, 60, 0, 360);
-mnAngle = map(mn, 0, 0, 60, 360);
-hrAngle = map(hr%12, 60, 0, 0, 360);
+mnAngle = map(mn, 0, 60, 0, 360);
+hrAngle = map(hr%12, 0, 12, 0, 360);
 
 push();
 rotate(scAngle);
 stroke(255,0,0);
 strokeWeight(7);
-line(0,0,100,0);
-pop();
-
-push();
-rotate(hrAngle);
-stroke(0,255,0);
-strokeWeight(7);
-line(0,0,100,0);
+line(0,0,130,0);
 pop();
 
 push();
@@ -41,6 +36,31 @@ strokeWeight(7);
 line(0,0,100,0);
 pop();
 
+push();
+rotate(hrAngle);
+stroke(0,255,0);
+strokeWeight(7);
+line(0,0,70,0);
+pop();
+
+stroke(255,0,255);
+point(0,0)
+
+//drawing the arcs  
+strokeWeight(10);
+noFill();
+
+ //Seconds 
+ stroke(255,0,0);
+  arc(0,0,300,300,0,scAngle);
+
+ //Minutes 
+ stroke(0,0,255); 
+ arc(0,0,280,280,0,mnAngle);
+
+ //Hour 
+ stroke(0,255,0);
+ arc(0,0,260,260,0,hrAngle);
 
   drawSprites();
 }
